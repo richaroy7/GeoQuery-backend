be863920731ca1d50d222cf0220ac5d6399b00b3
mainaak <mainaakch@gmail.com>
2023-12-19 10:58:40 +0530

    fix

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var os = require('os-utils');
 const express = require('express');
 const dotenv = require('dotenv');
 const errorHandler = require('./middleware/errorHandler');
@@ -6,8 +7,8 @@ const { connect } = require('mongoose');
 const connectDB = require('./config/dbConnection');
 dotenv.config();
 const morgan = require('morgan');
-const {createProxyMiddleware} = require("http-proxy-middleware");
-const cors  = require("cors");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+const cors = require("cors");
 
 //herewegoagain123
 
@@ -27,21 +28,29 @@ app.use(cors());
 app.use('/api/queries', require('./routes/queryRoute'));
 app.use('/api/data', require('./routes/dataRoute'));
 
-app.use('/api/users',require('./routes/userRoute'));
-app.use(
-    '/api/queries',
-    createProxyMiddleware({
-      target: 'http://127.0.0.1:5000',
-      changeOrigin: true,
-      logLevel: 'debug',
-      pathRewrite: {
-        '/api/processquery': 'http://127.0.0.1:5000/api/processquery',
-      },
-    }),
-);
-
-
-
-app.listen(port, () =>{
-    console.log(`Server running on port ${port}`);
+app.use('/api/users', require('./routes/userRoute'));
+app.get('/api/usage', async (req, res) => {
+  os.cpuUsage((v) => {
+    console.log('CPU Usage (%): ' + v);
+    return res.json({
+      usage: v*100.0,
+    });
+  })
 });
+  app.use(
+      '/api/queries',
+      createProxyMiddleware({
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+        logLevel: 'debug',
+        pathRewrite: {
+          '/api/processquery': 'http://127.0.0.1:5000/api/processquery',
+        },
+      }),
+  );
+
+
+
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
